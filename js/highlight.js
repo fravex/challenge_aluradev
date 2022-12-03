64153d65f1d1d86b58f99f9bb5f213909dc202ac
fravex <fravex@gmail.com>
2022-12-03 11:44:05 -0300

    sintaxe highlight

--- js/highlight.js
+++ js/highlight.js
@@ -1,7 +1,19 @@
-// hljs.highlightElement(document.getElementsByClassName('card_code')[0]);
+const btn_visualiza_highlight = document.getElementById('btn_visualiza_highlight');
+const input_linguagem = document.getElementById('linguagem');
+const card_code = document.getElementsByClassName('card_code')[0]
 
-btn_visualiza_highlight = document.getElementById('btn_visualiza_highlight');
 btn_visualiza_highlight.addEventListener('click', (e)=>{
     e.preventDefault();
+    classes_card_code = card_code.className;
+    pattern = /language\-\S+/g;
+    resultado = classes_card_code.match(pattern);
+    linguagem = input_linguagem.value;
+    if (resultado != null){
+        resultado.forEach(elemento =>{
+            card_code.classList.remove(elemento);
+        })
+    }
+    console.log(resultado);
+    card_code.classList.add(`language-${linguagem}`);
     hljs.highlightElement(document.getElementsByClassName('card_code')[0]);
 })
