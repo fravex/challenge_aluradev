ca19452c113dc105ab8af6470c95a09721938c05
fravex <fravex@gmail.com>
2022-12-04 08:29:53 -0300

    corrigindo alteracao de cores e sintaxe highlight

--- js/cards.js
+++ js/cards.js
@@ -1,7 +1,7 @@
 const card_projeto = document.getElementsByClassName('card_code')[0];
 const input_cor = document.getElementsByClassName('input_cor')[0];
 
-input_cor.addEventListener('click', (e)=>{
+input_cor.addEventListener('change', (e)=>{
     card_projeto.style.border = `${e.target.value} 1.5rem solid`;
     console.log(e.target.value)
 });
