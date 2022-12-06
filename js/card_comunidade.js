8c04690f7ed9fa4d5c7e38112d30357253c907d6
fravex <fravex@gmail.com>
2022-12-06 09:26:38 -0300

    cads comunidade js

---
+++ js/card_comunidade.js
@@ -0,0 +1,28 @@
+const main_page = document.getElementsByClassName('main_comunidade')[0];
+const card_footer = document.getElementsByClassName('card_comunidade_footer')
+
+cards = []
+
+for (let i = 0; i < main_page.childElementCount; i++) {
+    console.log(`card_` + (i+1))
+    cards.push(document.getElementById(`card_` + (i+1)))
+}
+
+
+for(let i = 0; i <main_page.childElementCount; i++){
+    card_footer[i].style.display = 'none';
+    
+    cards[i].addEventListener('mouseover', ()=>{
+        card_footer[i].style.display = 'flex';
+    })
+}
+
+
+for(let i = 0; i <main_page.childElementCount; i++){
+    
+    cards[i].addEventListener('mouseout', ()=>{
+        card_footer[i].style.display = 'none';
+    })
+
+
+}
